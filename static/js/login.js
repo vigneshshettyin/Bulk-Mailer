ee5ae6da7823712dfe2b8c8ae2bd91f2a7499164
Restyled.io <commits@restyled.io>
2021-03-26 05:50:47 +0000

    Restyled by prettier

--- static/js/login.js
+++ static/js/login.js
@@ -9,26 +9,24 @@ emailField.addEventListener("keyup", (e) => {
 
   if (emailVal.length > 0) {
     fetch("/validate/email", {
-      body : JSON.stringify({email : emailVal}),
-      method : "POST",
+      body: JSON.stringify({ email: emailVal }),
+      method: "POST",
     })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.email_valid) {
-            loginBtn.disabled = true;
-            emailField.classList.add("is-invalid");
-            emailFeedBackArea.style.display = "block";
-            emailFeedBackArea.innerHTML =
-                `<p class="text-center">You are not registered. Please sign up</p>`;
-          } else if (data.email_pattern_error) {
-            loginBtn.disabled = true;
-            emailField.classList.add("is-invalid");
-            emailFeedBackArea.style.display = "block";
-            emailFeedBackArea.innerHTML =
-                `<p class="text-center">${data.email_pattern_error}</p>`;
-          } else {
-            loginBtn.removeAttribute("disabled");
-          }
-        });
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.email_valid) {
+          loginBtn.disabled = true;
+          emailField.classList.add("is-invalid");
+          emailFeedBackArea.style.display = "block";
+          emailFeedBackArea.innerHTML = `<p class="text-center">You are not registered. Please sign up</p>`;
+        } else if (data.email_pattern_error) {
+          loginBtn.disabled = true;
+          emailField.classList.add("is-invalid");
+          emailFeedBackArea.style.display = "block";
+          emailFeedBackArea.innerHTML = `<p class="text-center">${data.email_pattern_error}</p>`;
+        } else {
+          loginBtn.removeAttribute("disabled");
+        }
+      });
   }
 });
